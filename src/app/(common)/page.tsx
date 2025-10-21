e761920fd6dcd3505bf68310f320e0dc1635b748
Sourave halder <souravh093@gmail.com>
2025-10-21 11:20:13 +0600

    make the eye care section done.

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import HeroSection from "./_components/HeroSection";
 import OurRelationSection from "./_components/OurRelationSection";
+import EyeCareSection from "./_components/EyeCareSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <OurRelationSection />
+      <EyeCareSection />
     </>
   );
 };
