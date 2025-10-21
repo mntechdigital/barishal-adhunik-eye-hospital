07f7265a523f47b5d7fb0f934b97a092050a15ed
reshad21 <miakala7@gmail.com>
2025-10-21 11:22:10 +0600

    create common section component

--- src/app/(common)/facility/page.tsx
+++ src/app/(common)/facility/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import TermsList from './_components/TermsList'
+import CommonSection from '@/components/shared/CommonSection'
 
 const Facilitypage = () => {
   return (
     <div>
+      <CommonSection/>
       <TermsList/>
     </div>
   )
---
+++ src/components/shared/CommonSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CommonSection = () => {
+  return (
+    <div>
+      <h2>Common Section</h2>
+      <p>This is a common section that can be reused across different pages.</p>
+    </div>
+  )
+}
+
+export default CommonSection
